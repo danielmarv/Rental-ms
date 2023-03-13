1aa24bf81de8d628cdaa7e71c53720adef703414
danielmarv <103020028+danielmarv@users.noreply.github.com>
2023-03-13 22:40:52 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
-import multer from "multer";
-import helmet from "helmet";
-import morgan from "morgan";
+// import express from "express";
+// import bodyParser from "body-parser";
+// import mongoose from "mongoose";
+// import cors from "cors";
+// import dotenv from "dotenv";
+// import multer from "multer";
+// import helmet from "helmet";
+// import morgan from "morgan";
 
 const http = require('http');
 
