0ed83c634eb861f27510f213b0c37da37671c317
danielmarv <103020028+danielmarv@users.noreply.github.com>
2023-03-13 22:39:10 +0300

    Initial Comit
    Starter React both frontend and backend

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+import helmet from "helmet";
+import morgan from "morgan";
+
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, world!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
