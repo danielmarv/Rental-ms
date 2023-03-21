a662066eacab2fcc3b4b9b5aadea8530f9927018
danielmarv <103020028+danielmarv@users.noreply.github.com>
2023-03-21 11:58:51 +0300

    @nivo/line
    @nivo/line

--- client/src/scenes/dashboard/index.jsx
+++ client/src/scenes/dashboard/index.jsx
@@ -15,11 +15,11 @@ import {
   useTheme,
   useMediaQuery,
 } from "@mui/material";
-// import { DataGrid } from "@mui/x-data-grid";
-// import BreakdownChart from "components/BreakdownChart";
-// import OverviewChart from "components/OverviewChart";
+import { DataGrid } from "@mui/x-data-grid";
+import BreakdownChart from "components/BreakdownChart";
+import OverviewChart from "components/OverviewChart";
 // import { useGetDashboardQuery } from "state/api";
-// import StatBox from "components/StatBox";
+import StatBox from "components/StatBox";
 
 const Dashboard = () => {
   const theme = useTheme();
